9028b7b1cff12b364b77af0f3c076558ac96fbdc
hexen16 <17078432+hexen16@user.noreply.gitee.com>
2026-06-02 22:13:10 +0800

    Initialize Spring Boot project

---
+++ src/main/java/com/example/zenvis/ZenvisApplication.java
@@ -0,0 +1,13 @@
+
+package com.example.zenvis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenvisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZenvisApplication.class, args);
+    }
+}
