484022a93e20ae0d46ec6686e092d5524f9cb67d
hexen16 <17078432+hexen16@user.noreply.gitee.com>
2026-06-03 20:14:07 +0800

    Add core application files

---
+++ src/main/java/com/coolxer/Application.java
@@ -0,0 +1,63 @@
+package com.coolxer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.AnnotationBeanNameGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+
+/**
+ * 启动类
+ *
+ * @author hunter
+ */
+@Slf4j
+@EnableScheduling
+@ServletComponentScan
+@SpringBootApplication(
+    exclude = {
+        DataSourceAutoConfiguration.class
+    }
+)
+public class Application {
+
+    /**
+     * 注入RestTemplate
+     */
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    /**
+     * 主方法.
+     */
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class)
+                .beanNameGenerator(new UniqueBeanNameGenerator())
+                .run(args);
+    }
+
+    public static class UniqueBeanNameGenerator extends AnnotationBeanNameGenerator {
+
+        /**
+         * 如果自定义了beanName，就取自定义的，不然取默认的
+         *
+         * @param definition
+         * @return
+         */
+        @Override
+        protected String buildDefaultBeanName(BeanDefinition definition) {
+            // 类名全路径
+            return definition.getBeanClassName();
+        }
+    }
+
+
+}
---
+++ src/main/java/com/coolxer/dao/mysql/entity/BaseEntity.java
@@ -0,0 +1,52 @@
+package com.coolxer.dao.mysql.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+/**
+ * 实体通用属性
+ *
+ * @author hunter
+ */
+@Data
+@Accessors(chain = true)
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @CreatedDate
+    @Column(name = "create_time", updatable = false, columnDefinition = "timestamp default current_timestamp")
+    private Date createTime;
+
+    @LastModifiedDate
+    @Column(name = "update_time", columnDefinition = "timestamp default current_timestamp")
+    private Date updateTime;
+
+    @CreatedBy
+    @Column(name = "create_by")
+    private Integer createBy;
+
+    @LastModifiedBy
+    @Column(name = "update_by")
+    private Integer updateBy;
+
+    /**
+     * 是否删除(仅逻辑删除时需要)
+     * 1:删除
+     * 0:默认
+     */
+    @Column(name = "is_delete", columnDefinition = "int default 0")
+    private Integer isDelete = 0;
+}
